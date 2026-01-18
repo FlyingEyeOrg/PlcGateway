a7760afe30b21d10d6d9ae6d29c4079aed3490ce
UnknowName <13604461032@163.com>
2026-01-18 16:02:02 +0800

    重命名倍福驱动类型。

---
+++ src/PlcGateway/Drivers/Beckhoff/BeckhoffDriverImplBase.cs
@@ -0,0 +1,58 @@
+﻿using PlcGateway.Core.Exceptions.YourNamespace.Exceptions;
+using System;
+using TwinCAT.Ads;
+
+namespace PlcGateway.Drivers.Beckhoff
+{
+    internal class BeckhoffDriverImplBase : IDisposable
+    {
+        public AmsNetId AmsNetId { get; }
+
+        public int Port { get; }
+
+        protected readonly AdsClient AdsClient = new AdsClient();
+
+        public bool IsConnected => AdsClient.IsConnected;
+
+        public BeckhoffDriverImplBase(AmsNetId amsNetId, int port)
+        {
+            AmsNetId = amsNetId;
+            Port = port;
+        }
+
+        public void Connect()
+        {
+            if (IsConnected)
+            {
+                return;
+            }
+
+            AdsClient.Connect(AmsNetId, this.Port);
+
+            if (AdsClient.IsConnected)
+            {
+                throw new BusinessException("");
+            }
+        }
+
+        public void Disconnect()
+        {
+            if (!IsConnected)
+            {
+                return;
+            }
+
+            AdsClient.Disconnect();
+
+            if (AdsClient.IsConnected)
+            {
+                throw new BusinessException("");
+            }
+        }
+
+        public void Dispose()
+        {
+            AdsClient.Dispose();
+        }
+    }
+}
